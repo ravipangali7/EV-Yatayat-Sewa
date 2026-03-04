07aa4b8ae636495d2be662972d30685edc6eaf35
Infelo <infelo453@gmail.com>
2026-03-04 15:00:04 +0545

    Changs

--- ev_yatayat_sewa_server/__init__.py
+++ ev_yatayat_sewa_server/__init__.py
@@ -1,3 +1,5 @@
 # Use PyMySQL as the MySQL driver for Django
 import pymysql
 pymysql.install_as_MySQLdb()
+# Django 6+ requires "mysqlclient 2.2.1+"; satisfy version check when using PyMySQL
+pymysql.version_info = (2, 2, 1, "final", 0)
